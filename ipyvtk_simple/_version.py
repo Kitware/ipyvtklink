2f6117d940376b7dee33fe375b49d345ead9fd86
banesullivan <bane.sullivan@kitware.com>
2020-08-20 09:45:36 -0600

    Bump Version: 0.0.1 -> 0.0.2

--- ipyvtk_simple/_version.py
+++ ipyvtk_simple/_version.py
@@ -1,6 +1,6 @@
 """Version info for ipyvtk_simple."""
 # major, minor, patch
-version_info = 0, 0, 1
+version_info = 0, 0, 2
 
 # Nice string for the version
 __version__ = ".".join(map(str, version_info))
