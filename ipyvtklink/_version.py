512160f0db2f5690d2b763fe07e8da40c6b9edde
banesullivan <banesulli@gmail.com>
2022-01-11 01:03:24 -0700

    Bump version: 0.2.1 -> 0.2.2

--- ipyvtklink/_version.py
+++ ipyvtklink/_version.py
@@ -1,6 +1,6 @@
 """Version info for ipyvtklink."""
 # major, minor, patch
-version_info = 0, 2, 1
+version_info = 0, 2, 2
 
 # Nice string for the version
 __version__ = ".".join(map(str, version_info))
